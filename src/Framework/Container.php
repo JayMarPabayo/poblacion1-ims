819ebf8f402729829325750f8b52aaf9a059eb44
JayMarPabayo <jaymarpabayo@gmail.com>
2024-01-09 04:08:46 +0800

    Invoking factory functions

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass, ReflectionNamedType;
+use Framework\Exceptions\ContainerException;
+
 class Container
 {
 
@@ -11,6 +14,42 @@ class Container
 
     public function addDefinitions(array $newDefinitions)
     {
-        dd($newDefinitions);
+        $this->definitions = [...$this->definitions, ...$newDefinitions];
+    }
+
+    public function resolve(string $classname)
+    {
+        $reflectionClass = new ReflectionClass($classname);
+        if (!$reflectionClass->isInstantiable()) {
+            throw new ContainerException("Class {$classname} is not instantiable");
+        }
+
+        $constructor = $reflectionClass->getConstructor();
+
+        if (!$constructor) {
+            return new $classname;
+        }
+
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $classname;
+        }
+
+        $dependancies = [];
+
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$classname} because param {$name} is missing a type hint");
+            }
+
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$classname} because invalid param type");
+            }
+        }
+        dd($params);
     }
 }
